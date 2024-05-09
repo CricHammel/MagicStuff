b3e218aba29c4c972c9a5b3f55cb1f0adc31ffe5
Cric_Hammel <cric_hammel@gmx.de>
2024-05-09 00:23:37 -0300

    Fixed .gitignore + Java 20 fix

--- src/main/java/de/cric_hammel/magicStuff/utilities/UpdateChecker.java
+++ src/main/java/de/cric_hammel/magicStuff/utilities/UpdateChecker.java
@@ -1,9 +1,8 @@
 package de.cric_hammel.magicStuff.utilities;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
+import java.net.URI;
 import java.net.URLConnection;
 
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,12 +20,12 @@ public class UpdateChecker {
 	public String getVersion() {
 		String urlString = "https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId;
 		try {
-			URLConnection connection = new URL(urlString).openConnection();
+			URLConnection connection = new URI(urlString).toURL().openConnection();
 			connection.setUseCaches(false);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			String version = reader.readLine();
 			return version;
-		} catch (IOException exception) {
+		} catch (Exception exception) {
 			plugin.getLogger().info("Unable to check for updates: " + exception.getMessage());
 		}
 		return null;
